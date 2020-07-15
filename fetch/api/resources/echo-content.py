ff69bbb65dbf57b216308370b8c123287ee67a61
Anne van Kesteren <annevk@annevk.nl>
2020-07-15 12:30:17 +0200

    Fetch: multi global tests
    For https://github.com/whatwg/fetch/issues/777.

--- fetch/api/resources/echo-content.py
+++ fetch/api/resources/echo-content.py
@@ -3,6 +3,7 @@ def main(request, response):
     headers = [("X-Request-Method", request.method),
                ("X-Request-Content-Length", request.headers.get("Content-Length", "NO")),
                ("X-Request-Content-Type", request.headers.get("Content-Type", "NO")),
+               ("X-Request-Referer", request.headers.get("Referer", "NO")),
                # Avoid any kind of content sniffing on the response.
                ("Content-Type", "text/plain")]
     content = request.body
