bbe9179413fcc499a70b86bb3a4f3b02c5110176
Adithya Srinivasan <adithyas@chromium.org>
2020-08-31 06:08:41 -0700

    Portals: Block cross-origin postMessage
    Updates implementation to drop messages sent to/from a portal that is
    cross origin with its host, and updates WPTs to reflect this change.
    
    Bug: 1108793
    Change-Id: Ie0056c7a01a4d65cccdbfb0a0044b3d6afc87497

--- fetch/metadata/resources/post-to-owner.py
+++ fetch/metadata/resources/post-to-owner.py
@@ -7,22 +7,27 @@ def main(request, response):
         (b"Content-Type", b"text/html"),
         (b"Cache-Control", b"no-cache, no-store, must-revalidate")
     ]
+    key = request.GET.first(b"key", None)
 
     body = u"""
         <!DOCTYPE html>
+        <script src="/portals/resources/stash-utils.sub.js"></script>
         <script>
             var data = %s;
             if (window.opener)
                 window.opener.postMessage(data, "*");
             if (window.top != window)
                 window.top.postMessage(data, "*");
-            if (window.portalHost)
-                window.portalHost.postMessage(data, "*");
+
+            const key = %s;
+            const value = JSON.stringify(data);
+            if (key && value)
+                StashUtils.putValue(key, value);
         </script>
-    """ % json.dumps({
+    """ % (json.dumps({
         u"dest": isomorphic_decode(request.headers.get(b"sec-fetch-dest", b"")),
         u"mode": isomorphic_decode(request.headers.get(b"sec-fetch-mode", b"")),
         u"site": isomorphic_decode(request.headers.get(b"sec-fetch-site", b"")),
         u"user": isomorphic_decode(request.headers.get(b"sec-fetch-user", b"")),
-        })
+        }), json.dumps(key))
     return headers, body
